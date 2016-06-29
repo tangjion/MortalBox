74154ec222c4ee184213a77d9b58fe134164c52b
huizuche-qd <qimortal@163.com>
2016-06-29 18:29:50 +0800

    es-promise 页面打字效果

--- src/polyfill-es6-promise/js/main.js
+++ src/polyfill-es6-promise/js/main.js
@@ -15,10 +15,48 @@ function timeout(time, obj) {
 //     }
 // });
 
-timeout(0).then(function(result) {
-    console.log("1212");
-    return timeout(1000);
+function inputText(id,txt) {
+    var ele = document.getElementById(id);
+    ele.innerHTML = txt;
+}
+
+timeout(0).then(function() {
+    inputText('row-01','|');
+    return timeout(300);
+}).then(function() {
+    inputText('row-01','t|');
+    return timeout(300);
 }).then(function () {
-    console.log('12');
-    return timeout(500);
+    inputText('row-01','th|');
+    return timeout(300);
+}).then(function () {
+    inputText('row-01','thi|');
+    return timeout(300);
+}).then(function () {
+    inputText('row-01','this|');
+    return timeout(300);
+}).then(function () {
+    inputText('row-01','this |');
+    return timeout(300);
+}).then(function () {
+    inputText('row-01','this a|');
+    return timeout(300);
+}).then(function () {
+    inputText('row-01','this a |');
+    return timeout(300);
+}).then(function () {
+    inputText('row-01','this a c|');
+    return timeout(300);
+}).then(function () {
+    inputText('row-01','this a co|');
+    return timeout(300);
+}).then(function () {
+    inputText('row-01','this a cod|');
+    return timeout(300);
+}).then(function () {
+    inputText('row-01','this a code|');
+    return timeout(300);
+}).then(function () {
+    inputText('row-01','this a code!|');
+    return timeout(300);
 });
